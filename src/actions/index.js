32a4a795fee03f91f1511d2cfaa49e2c1712ad4b
nndd91 <nndd91@hotmail.com>
2017-07-31 02:27:34 +0800

    Add api call

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,17 @@
+import axios from 'axios'
+
 const API_KEY=process.env.WT_API_KEY
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
 
 export const FETCH_WEATHER = 'FETCH_WEATHER'
 
 export function fetchWeather (city) {
-  const url = `${ROOT_URL}&q=${city},${sg}`
+  const url = `${ROOT_URL}&q=${city},sg`
+  const request = axios.get(url)
 
   return {
-    type: FETCH_WEATHER
+    type: FETCH_WEATHER,
+    payload: request
+
   }
 }
