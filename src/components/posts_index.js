5547968a662af0907d6a73eeabb76a02689c160e
NathanDev <nathan@get-assured.co>
2017-08-31 17:36:07 +0800

    Add in errors validation, Add in navigation

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import { fetchPosts } from '../actions'
+import { connect } from 'react-redux'
+import _ from 'lodash'
 
 class PostsIndex extends Component {
+  componentDidMount () {
+    this.props.fetchPosts()
+  }
+  renderPosts () {
+    return (
+      _.map(this.props.posts, (post) => {
+        return (
+          <div>
+            {post.title}
+            {post.category}
+          </div>
+        )
+      })
+    )
+  }
   render () {
     return (
       <div>
         Posts Index
+        <Link to='/new' className='btn btn-primary pull-right'>Add a Post</Link>
+        {this.renderPosts()}
       </div>
     )
   }
 }
 
-export default PostsIndex
+function mapStateToProps (state) {
+  return {
+    posts: state.posts
+  }
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex)
--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route } from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import promise from 'redux-promise'
+import logger from 'redux-logger'
 
-import reducers from './reducers';
+import reducers from './reducers'
 import PostsIndex from './components/posts_index'
+import PostsNew from './components/posts_new'
 
-const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise, logger)(createStore)
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
-      <div>
-        <Route path="/" component={PostsIndex} />
-      </div>
+      <Switch>
+        <Route exact path='/' component={PostsIndex} />
+        <Route path='/new' component={PostsNew} />
+      </Switch>
     </BrowserRouter>
   </Provider>
-  , document.querySelector('.container'));
+  , document.querySelector('.container'))
